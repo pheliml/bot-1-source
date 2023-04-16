f35b07f6755983712ba02e0cbe899963310954bf
pheliml <40262117+pheliml@users.noreply.github.com>
2023-04-16 16:36:30 +0100

    initial commit

--- bot_1_prod_source_code.py
+++ bot_1_prod_source_code.py
@@ -1,36 +1,41 @@
-import asyncio
-import json
 import websockets
+import json
 import pandas as pd
+import asyncio
 import ta
+import urllib.parse
 from binance.client import Client
+from binance.enums import *
+
+"""Initialise the Binance API call"""
+api_key = "mYKtwGFoTFYAR3XLPSW8oMyvK20iHjfOMUwsWnr6r3zSQ5awJ0PIieutZZ4G7dS7"
+api_secret = "hqFCEEps7OPaL7vsNeGJU0F55PwwCeLOgTjakpYI6wDpFAzMOWr2yFbAIoPl8fzL"
+client = Client(api_key, api_secret)
 
 
-#API Keys
-#API_KEY = <API_KEY>
-#API_SECRET = <API_SECRET>
-client = Client(API_KEY, API_SECRET)
-client.API_URL = 'https://api.binance.com'
+"""Test API request to server"""
+ping = client.ping()
+print(ping)
+status = client.get_system_status()
+print(status)
 
+"""Create the websocket connection"""
+stream =\
+websockets.connect('wss://stream.binance.com:9443/stream?streams=ethusdt@miniTicker')
 
-"""Function to create the dataframe to store the data from the websocket"""
+"""Create the dataframe to hold websocket information"""
 def createframe(msg):
     df = pd.DataFrame([msg])
-    #s=Symbol, E=EventTime, c=ClosingPrice
     df = df.loc[:,["s","E","c"]]
     df.columns = ["Symbol", "Time", "Price"]
     df.Price = df.Price.astype(float)
     df.Time = pd.to_datetime(df.Time, unit="ms")
     return df
 
-
-#Connect to websocket
-stream =\
-websockets.connect("wss://testnet.binance.vision/stream?streams=ethusdt@miniTicker")
-
+"""Create empty data frame to append new incoming data from the websocket to"""
 df=pd.DataFrame()
 
-#Main Loop
+"""Main loop"""
 async def main():
     sell_price = 0
     buy_price = 0
@@ -41,48 +46,52 @@ async def main():
             data = await reciever.recv()
             data = json.loads(data)["data"]
             df = df.append(createframe(data))
+            
             if len(df) >30:
                 if not open_position:
                     if ta.momentum.roc(df.Price, 30).iloc[-1] > 0 and \
                     ta.momentum.roc(df.Price, 30).iloc[-2]:
+                        # create a real order if the test orders did not raise an exception
                         try:
-                            buy_limit = client.create_test_order(
+                            order = client.create_order(
                             symbol='ETHUSDT',
                             side='BUY',
                             type='MARKET',
-                            quantity=10)
-                            print(buy_limit)
-                            buy_price = df.iloc[-1].Price
-                            print(f"Buy Price: {buy_price}")
-                            transact_time = df.iloc[-1].Time
+                            quantity=0.006)
+                            print(order)
                         except Exception as e:
                             print(e)
+                            break
                         open_position = True
-                        print(f"Trade Placed: {df.iloc[-1].Time}")
+                        buy_price = float(order["fills"][0]["price"])
+                        tether_buy_price = float(order["cummulativeQuoteQty"])
+                        print(f"Trade Placed - USDT:{tether_buy_price}")
+            
                 if open_position:
-                    subdf = df[df.Time >= transact_time]
+                    subdf = df[df.Time >= pd.to_datetime(order["transactTime"], unit="ms")]
                     if len(subdf) > 1:
                         subdf["highest"] = subdf.Price.cummax()
                         subdf["trailingstop"] = subdf["highest"] * 0.995
-                        if (subdf.iloc[-1].Price < subdf.iloc[-1].trailingstop).all() or \
-                        ((df.iloc[-1].Price / buy_price) > 1.002).all():
+                        if subdf.iloc[-1].Price < subdf.iloc[-1].trailingstop or \
+                        df.iloc[-1].Price / float(order["fills"][0]["price"]) >1.002:
                             try:
-                                sell_limit = client.create_test_order(
+                                order = client.create_order(
                                 symbol='ETHUSDT',
                                 side='SELL',
                                 type='MARKET',
-                                quantity=10)
-                                print(sell_limit)
-                                sell_price = subdf.iloc[-1].Price
-                                print(f"Option Sold at: {sell_price}")
+                                quantity=0.006)
+                                print(order)
                             except Exception as e:
-                                print(e) 
+                                print(e)
+                                break
+                            sell_price = float(order["fills"][0]["price"])
+                            tether_sell_price = float(order["cummulativeQuoteQty"])
+                            print(f"Trade Placed - USDT:{tether_sell_price}")
+                            print(f"You made {(tether_sell_price - tether_buy_price)} profit UDST")
                             open_position = False
-                            print(f"Trade Closed: {subdf.iloc[-1].Time}")
-                            print(f"Sold at: {sell_price} for a profit of {sell_price - buy_price}")
-                            #return sell_price, buy_price
                             break
-                        print(subdf.iloc[-1])        
+                        print(subdf.iloc[-1])
             if not open_position:
-                print(df.iloc[-1])                                 
+                print(df.iloc[-1])
+                
 asyncio.get_event_loop().run_until_complete(main())
